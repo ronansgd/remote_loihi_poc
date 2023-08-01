97e89722a8e5d7eef1c7526ce600657cb72f4759
Ronan Sangouard <sangouard@fortiss.org>
2023-08-01 19:34:20 +0200

    WIP: implementing basic protocol

--- snippets/numpy_client.py
+++ snippets/numpy_client.py
@@ -5,22 +5,25 @@ import socket
 
 import numpy as np
 
+import _utils
+
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 SHAPE = (10,)
 DTYPE = np.int32
+NDIM = len(SHAPE)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
 
-    arr = np.zeros(SHAPE, DTYPE)
-    arr_bytes = arr.tobytes()
-    s.sendall(arr_bytes)
-    print(f"Sent {arr}")
+        arr = np.zeros(SHAPE, DTYPE)
+        arr_bytes = arr.tobytes()
+        s.sendall(arr_bytes)
+        print(f"Sent {arr}")
 
-    rec_arr_bytes = s.recv(len(arr_bytes))
-    rec_arr = np.frombuffer(rec_arr_bytes, dtype=DTYPE).reshape(SHAPE)
+        rec_arr_bytes = s.recv(_utils.get_numpy_bytes_len(DTYPE, SHAPE))
+        rec_arr = np.frombuffer(rec_arr_bytes, dtype=DTYPE).reshape(SHAPE)
 
-
-print(f"Received {rec_arr}")
+    print(f"Received {rec_arr}")
--- snippets/numpy_server.py
+++ snippets/numpy_server.py
@@ -4,6 +4,8 @@
 import socket
 import numpy as np
 
+import _utils
+
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
@@ -12,24 +14,22 @@ SHAPE = (10,)
 DTYPE = np.int32
 
 
-def get_numpy_bytes_len(dtype: np.dtype, shape: tuple) -> int:
-    return np.dtype(dtype).itemsize * np.prod(shape, dtype=np.int64)
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
 
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    arr_bytes = conn.recv(
+                        _utils.get_numpy_bytes_len(DTYPE, SHAPE))
+                    if not arr_bytes:
+                        break
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+                    arr = np.frombuffer(arr_bytes, dtype=DTYPE).reshape(SHAPE)
+                    print(f"Received array {arr}")
 
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                arr_bytes = conn.recv(get_numpy_bytes_len(DTYPE, SHAPE))
-                if not arr_bytes:
-                    break
-
-                arr = np.frombuffer(arr_bytes, dtype=DTYPE).reshape(SHAPE)
-                print(f"Received array {arr}")
-
-                conn.sendall(arr.tobytes())
+                    conn.sendall(arr.tobytes())
