c16a1d3efbea9b2272b676bfd30434d6b12b86ce
Ronan Sangouard <sangouard@fortiss.org>
2023-08-01 17:46:57 +0200

    Improved numpy byte encoding

--- snippets/encode_numpy.py
+++ snippets/encode_numpy.py
@@ -1,9 +1,17 @@
 import numpy as np
 
-if __name__ == "__main__":
 
-    arr = np.zeros(10)
+if __name__ == "__main__":
+    # init array
+    shape, dtype = (10, ), np.int64
+    arr = np.zeros(shape, dtype)
     print(arr)
+
+    # encode to byte string & check size
     arr_bytes = arr.tobytes()
-    rec_arr = np.frombuffer(arr_bytes, dtype=arr.dtype).reshape(arr.shape)
+    theo_bytes_size = np.dtype(dtype).itemsize * np.prod(shape, dtype=np.int64)
+    assert len(arr_bytes) == theo_bytes_size
+
+    # reconstruct array from byte string
+    rec_arr = np.frombuffer(arr_bytes, dtype=dtype).reshape(shape)
     print(rec_arr)
