5f897f48b7d49abe73d554fa498a85c239e8c575
Ronan Sangouard <sangouard@fortiss.org>
2023-08-01 17:37:01 +0200

    Added snippet for remote SSH bash execution

---
+++ snippets/remote_ssh_exec.py
@@ -0,0 +1,26 @@
+# https://www.linuxjournal.com/content/using-named-pipes-fifos-bash
+
+import subprocess
+
+sshProcess = subprocess.Popen(['ssh',
+                               '-tt',
+                               'ncl-com'],
+                              stdin=subprocess.PIPE,
+                              stdout=subprocess.PIPE,
+                              universal_newlines=True,
+                              bufsize=0)
+sshProcess.stdin.write("ls .\n")
+sshProcess.stdin.write("echo END\n")
+sshProcess.stdin.write("uptime\n")
+sshProcess.stdin.write("logout\n")
+sshProcess.stdin.close()
+
+
+for line in sshProcess.stdout:
+    if line == "END\n":
+        break
+    print(line, end="")
+
+# to catch the lines up to logout
+for line in sshProcess.stdout:
+    print(line, end="\n")
