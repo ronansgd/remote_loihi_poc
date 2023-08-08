adde5fbdd69d13df93a4e17afb5f3ef4e8e038f5
Ronan Sangouard <sangouard@fortiss.org>
2023-08-08 12:07:54 +0200

    Hacky script for generating two port ids

--- forward_port.sh
+++ forward_port.sh
@@ -1,6 +1,9 @@
 echo "Enter your ncl-com username: "  
 read USER_NAME  
 
-LOIHI_PORT=$(shuf -i 1024-65535 -n 1)
-echo "Generated port: $LOIHI_PORT"
-ssh -t -L $LOIHI_PORT:127.0.0.1:$LOIHI_PORT $USER_NAME@ncl-com.research.intel-research.net "export LOIHI_PORT=$LOIHI_PORT; bash -l"
+MGMT_PORT=$(shuf -i 1024-65535 -n 1)
+echo "Management port: $MGMT_PORT"
+
+DATA_PORT=$(shuf -i 1024-65535 -n 1)
+echo "Data port: $DATA_PORT"
+ssh -t -L $MGMT_PORT:127.0.0.1:$MGMT_PORT -L $DATA_PORT:127.0.0.1:$DATA_PORT $USER_NAME@ncl-com.research.intel-research.net "export MGMT_PORT=$MGMT_PORT; export DATA_PORT=$DATA_PORT; bash -l"
